ce20ce8163a3a937e0352f611e49fdd36ff64fe9
Rafael Luis Guerra <rafael.guerra@dextra-sw.com>
2019-06-06 15:02:09 -0300

    Update Customer Endpoint

--- demowebflux/src/main/java/com/example/webflux/controllers/CustomerController.java
+++ demowebflux/src/main/java/com/example/webflux/controllers/CustomerController.java
@@ -21,6 +21,12 @@ public class CustomerController {
         return customerRepository.findAll();
     }
 
+    @GetMapping(path = "/count")
+    public Mono<Long> count() { return customerRepository.count(); }
+
+    @GetMapping(path = "/j")
+    public Flux<Customer> findCustomersStartWithJ() { return customerRepository.findCustomersStartWithJ(); }
+
     @PostMapping(path = "/new")
     public Mono<Customer> save(@RequestBody Customer customer) {
         return customerRepository.save(customer);
